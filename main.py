09bdfa7a6a7ed4a07774f6c0fda3d907d71401a3
Arsham <Arshamalh@gmail.com>
2021-10-18 00:54:16 +0330

    Step 2

--- main.py
+++ main.py
@@ -1,57 +1,61 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, dispatcher
 from random import choice
 
 # Bot settings
 BotToken = "1987640815:AAEdOu-w47RcYRCFoTQognLmg6QKZDvkgPA"
-updater = Updater(token=BotToken, use_context=True)
-dispatcher = updater.dispatcher
+Bot = Updater(token=BotToken, use_context=True)
+dispatcher = Bot.dispatcher
 
 # Core settings
 states = ['r', 'p', 's']
 Shaper = {"r": "💎︁",
-        "p": "📜︁",
-        "s": "✀︁"}
+          "p": "📜︁",
+          "s": "✀︁"}
+
 win_rates = {}
 
-def start(update, context):
-    win_rates[update.effective_chat.id] = {"c_wins": 0, "u_wins": 0}
-    context.bot.send_message(chat_id=update.effective_chat.id, 
-        text=("Welcome to RPS game"
-            "\nYou can start playing by typing your chance of rock, paper, scissors (or r, p, s)"
-            "\nif you want to exit, just type 'exit'"
-            "\nif you want to play again from zero, just type 'again'"))
+# Commands definition
+def start(update, ctx):
+    win_rates[update.effective_chat.id] = {'c_wins':0, 'u_wins': 0}
+    ctx.bot.send_message(chat_id=update.effective_chat.id, 
+    text=("Welcome to RPS game"
+        "\nYou can start playing by typing your chance of rock, paper, scissors (or r, p, s)"
+        "\nif you want to exit, just type 'exit'"
+        "\nif you want to play again from zero, just type 'again'"
+    ))
 
-def inputer(update, context):
+def inputHandler(update, ctx):
     text = checker(update.effective_chat.id, update.message.text)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+    ctx.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
 def checker(uid, text):
     inp = text.lower()[0]
     c_choice = choice(states)
     if inp == 'e':
-        win_rates[uid] = {"c_wins": 0, "u_wins": 0}
-        return 'Thank you for playing with us!\nYou can /start again anytime you want!'
+        win_rates[uid] = {'c_wins':0, 'u_wins': 0}
+        return 'Thank you for playing with us!'
     
     elif inp == 'a': 
-        win_rates[uid] = {"c_wins": 0, "u_wins": 0}
+        win_rates[uid] = {'c_wins':0, 'u_wins': 0}
         return "Restarted!"
 
     elif (inp == 'r' and c_choice == 'p'
     ) or (inp == 'p' and c_choice == 's'
-    ) or (inp == 's' and c_choice == 'r'): win_rates[uid]["c_wins"] += 1
+    ) or (inp == 's' and c_choice == 'r'): win_rates[uid]['c_wins'] += 1
 
     elif (inp == 'r' and c_choice == 's'
     ) or (inp == 'p' and c_choice == 'r'
-    ) or (inp == 's' and c_choice == 'p'): win_rates[uid]["u_wins"] += 1
+    ) or (inp == 's' and c_choice == 'p'): win_rates[uid]['u_wins'] += 1
 
-    else: pass
+    elif inp == c_choice: pass
+    
+    else: return "Wrong input! Please try again."
 
-    return f' User Wins: {win_rates[uid]["u_wins"]} \n Computer Wins: {win_rates[uid]["c_wins"]} \n You {Shaper[inp]}  vs {Shaper[c_choice]}  Computer'
+    return f'User Wins: {win_rates[uid]["u_wins"]} \nComputer Wins: {win_rates[uid]["c_wins"]} \n You {Shaper[inp]}  vs {Shaper[c_choice]}  Computer'
 
+# Add handlers
 dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(MessageHandler(Filters.text, inputer))
+dispatcher.add_handler(MessageHandler(Filters.text, inputHandler))
 
-updater.start_polling()
-
-
-
+# Start polling!
+Bot.start_polling()
