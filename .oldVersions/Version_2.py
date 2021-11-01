015e00547a19345ca6c470ba3c2ced980870b888
Arsham <Arshamalh@gmail.com>
2021-11-01 14:03:04 +0330

    .

--- .oldVersions/Version_2.py
+++ .oldVersions/Version_2.py
@@ -24,30 +24,6 @@ Shaper = {"r": "💎︁",
 
 win_rates = {}
 
-# Logic
-def checker(uid, text):
-    inp = text.lower()[0]
-    c_choice = choice(states)
-    if inp == 'a':
-        win_rates[uid] = {'c_wins':0, 'u_wins': 0}
-        return 'Restarted!'
-
-    elif (inp == 'r' and c_choice == 'p'
-    ) or (inp == 'p' and c_choice == 's'
-    ) or (inp == 's' and c_choice == 'r'): win_rates[uid]['c_wins'] += 1
-
-    elif (inp == 'r' and c_choice == 's'
-    ) or (inp == 'p' and c_choice == 'r'
-    ) or (inp == 's' and c_choice == 'p'): win_rates[uid]['u_wins'] += 1
-
-    elif inp == c_choice: pass
-    
-    else: return "Wrong input! Please try again."
-
-    return (f'User Wins: {win_rates[uid]["u_wins"]}'
-            f'\nComputer Wins: {win_rates[uid]["c_wins"]} '
-            f'\n You {Shaper[inp]}  vs {Shaper[c_choice]}  Computer')
-
 # Commands
 def start(update, ctx):
     win_rates[update.effective_chat.id] = {'c_wins':0, 'u_wins': 0}
@@ -82,6 +58,29 @@ def ButtonHandler(update, ctx):
     ctx.user_data['last_message_id'] = query.message.message_id
     query.edit_message_text(text=text, reply_markup=PlayingKeyboard)
 
+# Logic
+def checker(uid, text):
+    inp = text.lower()[0]
+    c_choice = choice(states)
+    if inp == 'a':
+        win_rates[uid] = {'c_wins':0, 'u_wins': 0}
+        return 'Restarted!'
+
+    elif (inp == 'r' and c_choice == 'p'
+    ) or (inp == 'p' and c_choice == 's'
+    ) or (inp == 's' and c_choice == 'r'): win_rates[uid]['c_wins'] += 1
+
+    elif (inp == 'r' and c_choice == 's'
+    ) or (inp == 'p' and c_choice == 'r'
+    ) or (inp == 's' and c_choice == 'p'): win_rates[uid]['u_wins'] += 1
+
+    elif inp == c_choice: pass
+    
+    else: return "Wrong input! Please try again."
+
+    return (f'User Wins: {win_rates[uid]["u_wins"]}'
+            f'\nComputer Wins: {win_rates[uid]["c_wins"]} '
+            f'\n You {Shaper[inp]}  vs {Shaper[c_choice]}  Computer')
 
 # Bot settings
 BotToken = denv()['BotToken']
--- changelist_V2.md
+++ changelist_V2.md
@@ -1,6 +1,9 @@
-## 1 - Gameplay enhanced 
+## 1 - Scissor shape changed!
+## 2 - Exiting the game was pointless and removed.
+## 3 - Bot stopping with ctrl + c fixed.
+
+## 4 - Token in .env file 
+
+## 4 - Gameplay enhanced 
 Now we accept buttons as well.
 And to have a clean page, we delete text messages after three seconds.
-## 2 - Scissor shape changed!
-## 3 - Exiting the game was pointless and removed.
-## 4 - Bot stopping with ctrl + c fixed.
